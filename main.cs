29329fa04758bd2c04cd7e03c729664d5d1d4415
EdgarsBe <edgarsberzins100@gmail.com>
2022-04-11 20:16:01 +0300

    Nahuj vs 2019 huiņa pedejā

--- main.cs
+++ main.cs
@@ -1,9 +1,21 @@
 using System;
-					
-public class Program
+
+namespace Project
 {
-	public static void Main()
+	public class Program
 	{
-		Console.WriteLine("Hello World");
+		static void Main(string[] args)
+		{
+
+		}
+
+		private static void GalvenaIzvelne()
+		{
+			Console.WriteLine("----------------------------------------");
+			Console.WriteLine("|INTERNETA VEIKALA INFORMACIJAS SISTEMA|");
+			Console.WriteLine("----------------------------------------");
+			Console.WriteLine("Galvena izvelne: ");
+			Console.WriteLine("1.) Pievienot Datus");
+		}
 	}
 }
